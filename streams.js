2c5e7d58251dae49cef9f0d1257f1301b0ea8318
donkeykong91 <darylnator@gmail.com>
2018-07-02 05:41:01 -0700

    Added three event listeners, one to indicate the start of the read, another to show the chunks grabbed, and a third to show the end of readig the stream

--- streams.js
+++ streams.js
@@ -1,7 +1,22 @@
 var fs = require("fs");
 
-fs.readFile("./chat.log", "UTF-8", function (err, chatlog) {
-  console.log(`File Read ${chatlog.length}`);
+var stream = fs.createReadStream("./chat.long", "UTF-8");
+
+var data = "";
+
+stream.once("data", function () {
+  console.log("\n\n\n");
+  console.log("Started Reading File");
+  console.log("\n\n\n");
 });
 
-console.log("Reading File");
+stream.on("data", function (chunk) {
+  process.stdout.write(` chunk: ${chunk.length} |`);
+  data += chunk;
+});
+
+stream.on("end", function () {
+  console.log("\n\n\n");
+  console.log(`Finished Reading File ${data.length}`);
+  console.log("\n\n\n");
+});
